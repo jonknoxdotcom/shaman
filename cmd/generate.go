4b83a42ed91d79840258e974610f8fbabf1d1048
Jon Knox <me@jonknox.com>
2025-07-23 18:35:57 +0100

    fix: reporting switches for generate

--- cmd/generate.go
+++ cmd/generate.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 
@@ -20,15 +21,18 @@ import (
 )
 
 // Local variables
-var cli_path string = ""        // Path to folder where scan will be performed [cobra]
-var cli_anon bool = false       // Path to folder where scan will be performed [cobra]
-var dupes = map[string]uint32{} // duplicates (collected during walk)
+var cli_path string = ""     // Path to folder where scan will be performed [cobra]
+var cli_anon bool = false    // Anonymise the output (discard file, modified time and size)
+var cli_dupes bool = false   // Show duplicates as comments at end of run
+var cli_totals bool = false  // Show files/bytes total at end of run
+var dupes = map[string]int{} // duplicates (collected during walk)
 
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate [file.ssf]",
 	Short: "Generate a sha-manager signature format (.ssf) file",
-	Long: `Generate a sha-manager format (.ssf) file from specified directory (or current directory if none specified), 
+	Long: `shaman generate
+Generate a sha-manager format (.ssf) file from specified directory (or current directory if none specified), 
 writing the output to a named file (or stdout if none given)`,
 	Aliases: []string{"gen"},
 	Args:    cobra.MaximumNArgs(1),
@@ -42,6 +46,8 @@ func init() {
 
 	generateCmd.Flags().StringVarP(&cli_path, "path", "p", "", "Path to directory to scan (default is current directory)")
 	generateCmd.Flags().BoolVarP(&cli_anon, "anonymous", "a", false, "Whether to mask the SSF output (to include only hashes)")
+	generateCmd.Flags().BoolVarP(&cli_dupes, "dupes", "d", false, "Whether to show dupes (as comments) on completion")
+	generateCmd.Flags().BoolVarP(&cli_totals, "totals", "t", false, "Display count of bytes and files on completion")
 }
 
 // ----------------------- Generate function below this line -----------------------
@@ -67,13 +73,14 @@ func abort(rc int, reason string) {
 	os.Exit(rc)
 }
 
-func WalkTree(startpath string) (int64, error) {
+func WalkTree(startpath string) (int64, int64, error) {
 	// uses the "new" (1.16) os.ReadPath functionality
 	entries, err := os.ReadDir(startpath)
 	if err != nil {
 		abort(1, "Unrecoverable failure to read directory")
 	}
-	var total int64
+	var total_files int64
+	var total_bytes int64
 	for _, entry := range entries {
 		if !entry.IsDir() {
 			// we ignore symlinks
@@ -86,47 +93,38 @@ func WalkTree(startpath string) (int64, error) {
 			if err != nil {
 				abort(2, "Internal error #2")
 			}
-
 			size := info.Size()
-			unixtime := info.ModTime().Unix()
-			// mode := info.Mode() // looks like '-rwxr-xr-x', alsoi synonymous to entry.Type().Perm()
 
-			sha, _ := GetSha256OfFile(name)
-			shab64 := b64.StdEncoding.EncodeToString(sha)
-			if len(shab64) != 44 {
+			sha_bin, _ := GetSha256OfFile(name)
+			sha_b64 := b64.StdEncoding.EncodeToString(sha_bin)
+			if len(sha_b64) != 44 || sha_b64[43:] != "=" {
 				// can't happen
 				abort(3, "Internal error #3: "+name)
 			}
-			if shab64[43:] != "=" {
-				// can't happen
-				abort(4, "Internal error #4: "+name)
-			} else {
-				shab64 = shab64[0:43]
-			}
-			dupes[shab64] = dupes[shab64] + 1
+			sha_b64 = sha_b64[0:43]
+			dupes[sha_b64] = dupes[sha_b64] + 1
 
 			if cli_anon {
-				fmt.Println(shab64)
+				fmt.Println(sha_b64)
 			} else {
-				fmt.Printf("%s%x%04x :%s", shab64, unixtime, size, name)
+				unixtime := info.ModTime().Unix()
+				// mode := info.Mode() // looks like '-rwxr-xr-x', alsoi synonymous to entry.Type().Perm()
+				fmt.Printf("%s%x%04x :%s", sha_b64, unixtime, size, name)
 				fmt.Println()
 			}
-		}
-		if entry.IsDir() {
-			size, err := WalkTree(path.Join(startpath, entry.Name()))
-			if err != nil {
-				return 0, err
-			}
-			total += size
+			total_bytes += size
+			total_files++
 		} else {
-			info, err := entry.Info()
+			// it's a directory - traverse
+			files, size, err := WalkTree(path.Join(startpath, entry.Name()))
 			if err != nil {
-				return 0, err
+				return 0, 0, err
 			}
-			total += info.Size()
+			total_files += files
+			total_bytes += size
 		}
 	}
-	return total, nil
+	return total_files, total_bytes, nil
 }
 
 func gen(args []string) {
@@ -143,15 +141,35 @@ func gen(args []string) {
 	}
 
 	// Call the tree walker
-	_, _ = WalkTree(startpath)
+	tf, tb, err := WalkTree(startpath)
+	if err != nil {
+		abort(5, "Internal error #5: ")
+	}
+
+	// Optional totals statement
+	if cli_totals {
+		fmt.Printf("# %d files, %d bytes", tf, tb)
+		fmt.Println()
+	}
 
 	// This directory reader uses the new os.ReadDir (req 1.16)
 	// https://benhoyt.com/writings/go-readdir/
 
-	// for id, times := range dupes {
-	// 	if times > 1 {
-	// 		fmt.Println("# " + id)
-	// 	}
-	// }
+	// Optional duplicates statement
+	done_header := false
+	if cli_dupes {
+		for id, times := range dupes {
+			if times > 1 {
+				if !done_header {
+					fmt.Println("# ----------------- Duplicates -----------------")
+					done_header = true
+				}
+				fmt.Println("# " + id + " x" + strconv.Itoa(times))
+			}
+		}
+		if !done_header {
+			fmt.Println("# There were no duplicates")
+		}
+	}
 
 }
